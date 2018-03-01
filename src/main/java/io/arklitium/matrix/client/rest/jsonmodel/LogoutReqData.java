6f2cdbee8d3a1e9ac540732f920c3cf8d16f4f6c
adrien <adrien@kamax.io>
2018-03-01 07:05:30 +0100

    added empty toString to logout req data (post but with no data defined)

--- src/main/java/io/arklitium/matrix/client/rest/jsonmodel/LogoutReqData.java
+++ src/main/java/io/arklitium/matrix/client/rest/jsonmodel/LogoutReqData.java
@@ -2,5 +2,11 @@ package io.arklitium.matrix.client.rest.jsonmodel;
 
 public class LogoutReqData {
 
+    public LogoutReqData() {
+    }
 
+    @Override
+    public String toString() {
+        return "LogoutReqData{}";
+    }
 }
