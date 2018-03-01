000a1ea94035917c2231fcef9527569c370af4ce
adrien <adrien@kamax.io>
2018-03-01 04:19:12 +0100

    added 'medium' and 'address' to login request message

--- src/main/java/io/arklitium/matrix/client/rest/jsonmodel/LoginReqData.java
+++ src/main/java/io/arklitium/matrix/client/rest/jsonmodel/LoginReqData.java
@@ -3,10 +3,18 @@ package io.arklitium.matrix.client.rest.jsonmodel;
 public class LoginReqData {
     private String type = "m.login.password";
     private String user;
+    private String medium;
+    private String address;
     private String password;
 
     public LoginReqData(String user, String password) {
         this.user = user;
+        this.password = password;
+    }
+
+    public LoginReqData(String medium, String address, String password) {
+        this.medium = medium;
+        this.address = address;
         this.password = password;
     }
 
@@ -18,16 +26,25 @@ public class LoginReqData {
         return user;
     }
 
+    public String getMedium() {
+        return medium;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
     public String getPassword() {
         return password;
     }
 
-
     @Override
     public String toString() {
-        return "LoginData{" +
+        return "LoginReqData{" +
                 "type='" + type + '\'' +
                 ", user='" + user + '\'' +
+                ", medium='" + medium + '\'' +
+                ", address='" + address + '\'' +
                 ", password='" + password + '\'' +
                 '}';
     }
